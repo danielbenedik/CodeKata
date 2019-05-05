e56401ccd4a5748a07f83cfa75a3c59d10eda8ee
daniel benedik <45199837+danielbenedik@users.noreply.github.com>
2019-05-05 18:54:26 +0300

    Add files via upload

---
+++ Highest and Lowest/Highest and Lowest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Kata
+{
+    public static void Main()
+    {
+        string str = "-1 2 3 4 45";
+        Console.WriteLine(HighAndLow(str));
+    }
+
+   
+    public static string HighAndLow(string numbers)
+    {
+
+        string[] allParts = numbers.Split(' ');
+
+        int num = int.Parse(allParts[0]);
+        int maxNum = num, minNum = num;
+
+        for (int i = 1; i < allParts.Length; i++)
+        {
+            num = int.Parse(allParts[i]);
+
+            if (minNum < num)
+                minNum = num;
+
+            if (maxNum > num)
+                maxNum = num;
+        }
+
+        string strMaxNum = maxNum.ToString();
+        string strMinNum = minNum.ToString();
+
+        string res = minNum.ToString() + " " + maxNum.ToString();
+
+        return res;
+
+    }
+
+}
