b480b1dd022d089ebe385a17a3d8219944799744
daniel benedik <45199837+danielbenedik@users.noreply.github.com>
2019-05-05 16:02:49 +0300

    Add files via upload

---
+++ Equal Sides Of An Array/Equal Sides Of An Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Kata
+{
+    public static void Main()
+    {
+        int[] arr2 = { 20, 10, 30, 10, 10, 15, 35 };
+        Console.WriteLine(FindEvenIndex(arr2));
+    }
+
+    public static int FindEvenIndex(int[] arr)
+    {
+        
+        int countLeft = 0;
+        int countRithg = arr.Sum();
+
+        for (int i=0; i < arr.Length; i++)
+        {
+
+
+            countLeft += arr[i];
+            if (i > 0)
+                countRithg -= arr[i - 1];
+
+            if (countLeft == countRithg)
+                return (i);
+
+        }
+            return (-1);
+
+    }
+}
